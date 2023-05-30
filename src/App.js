094c438ee077fe6c60ac41272c33f9bcb8febef7
JihyeJayPark <jihyepark1478@gmail.com>
2023-05-29 20:07:35 -0400

    drafted the loading page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Project 3</h1>
+        <h1>Morning Routine Tracker</h1>
+        <p>Are you ready for your morning?</p>
+        <a>See your tracker!</a>
       </header>
     </div>
   );
